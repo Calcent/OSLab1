f71a99b2c2309bfb088057bd29d14e4e375bacd1
Calcent <134114315+Calcent@users.noreply.github.com>
2025-04-23 11:56:55 -0700

    added if statement

--- lab1.sh
+++ lab1.sh
@@ -7,7 +7,11 @@ mkdir "$dirname"
 
 for i in {0..9}; do
 	subname=$((i+1))
-	mkdir $dirname/"file10$subname"
+	if [ "$i" -eq 9 ]; then
+    		mkdir "$dirname/file11$subname"
+	else
+    		mkdir "$dirname/file10$subname"
+	fi
 	echo "Hello again you dying world","$subname"
 done
 
